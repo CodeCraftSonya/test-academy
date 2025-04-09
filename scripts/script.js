642d8097a067610e8b9930d866760717333ef72d
DESKTOP-L88LUBN\София <sofiya.fish02@mail.ru>
2025-04-09 17:56:33 +0300

    feat: add adaptive design for 720px and 360px

--- scripts/script.js
+++ scripts/script.js
@@ -5,11 +5,37 @@ const template = document.getElementById("cardTemplate");
 
 cardsData.items.forEach((item) => {
   const clone = template.content.cloneNode(true);
+
   clone.querySelector(".content__card-title").innerHTML = item.title.replace(
     /\n/g,
     "<br>",
   );
   clone.querySelector(".content__card-description").textContent = item.text;
-  clone.querySelector(".content__card-date").textContent = item["date-state"];
+
+  const dateElement = clone.querySelector(".content__card-date");
+  const dateWrapper = clone.querySelector(".content__card-date-wrapper");
+
+  let icon = null;
+
+  if (item["date-state"] === "Идет трансляция") {
+    icon = document.createElement("img");
+    icon.src = "../icons/solar_play-stream-broken.svg";
+    icon.alt = "Онлайн";
+    icon.className = "date-icon";
+
+    dateElement.classList.add("date--live");
+  } else if (item["date-state"].startsWith("Начнется через")) {
+    icon = document.createElement("img");
+    icon.src = "../icons/solar_check-square-broken.svg";
+    icon.alt = "Ожидание";
+    icon.className = "date-icon";
+  }
+
+  if (icon) {
+    dateWrapper.insertBefore(icon, dateElement);
+  }
+
+  dateElement.textContent = item["date-state"];
+
   container.appendChild(clone);
 });
